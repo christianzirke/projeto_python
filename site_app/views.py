1b32b677bd058cdfa21baf471129c73b7ecc762b
Christian Osta <christianzirke@gmail.com>
2017-06-11 13:31:14 -0300

    Integracao da wiki na view

--- site_app/views.py
+++ site_app/views.py
@@ -5,9 +5,12 @@ from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.http import Http404 
 
+import wikipedia
+
 from .models import Company
 
 # Create your views here.
 def company_details(request, company_id):
 	company = get_object_or_404(Company, pk=company_id)
-	return render(request, 'site_app/index.html', {"company": company})
+	wiki = wikipedia.page(company.name)
+	return render(request, 'site_app/index.html', {"company": company, "wiki": wiki})
